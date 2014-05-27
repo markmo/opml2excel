149a69d3595be17f7f3b3caa403dc011a17f07d9
Mark Moloney <markmo@me.com>
2014-05-28 08:29:44 +1000

    initial xls writer

--- build.sbt
+++ build.sbt
@@ -1,4 +1,5 @@
 name := "opml2excel"
 
 version := "1.0"
-    
+
+libraryDependencies += "org.apache.poi" % "poi" % "3.10-FINAL"
--- src/main/scala/Opml2Excel.scala
+++ src/main/scala/Opml2Excel.scala
@@ -1,3 +1,5 @@
+import java.io.FileOutputStream
+import org.apache.poi.hssf.usermodel.{HSSFSheet, HSSFWorkbook}
 import scala.xml.{NodeSeq, XML}
 
 /**
@@ -5,34 +7,89 @@ import scala.xml.{NodeSeq, XML}
  */
 object Opml2Excel extends App {
 
-  case class Row(name: String, note: String, children: Seq[Row], level: Int) {
+  case class Row(name: String, note: String, children: Seq[Row], rowNum: Int, level: Int) {
     override def toString = {
-      " " * level * 4 + name + "\n" + children.mkString("")
+      rowNum + " " * level * 4 + name + "\n" + children.mkString("")
     }
   }
 
-  case class Column(name: String, values: List[ColumnValue])
+  case class Column(name: String, values: List[ColumnValue]) {
+    def getValue(rowNum: Int) = {
+      val a = values.filter(_.rowNum == rowNum)
+      if (a.isEmpty) {
+        null
+      } else {
+        a.head.value
+      }
+    }
+  }
 
   case class ColumnValue(rowNum: Int, value: String)
 
   val columns = collection.mutable.Map[String, Column]()
 
-  def parseOpml(s: NodeSeq, rowNum: Int, level: Int): Seq[Row] = {
+  def parseOpml(s: NodeSeq, rowNum: Int, level: Int): (Seq[Row], Int) = {
     if (s.isEmpty) {
-      return Nil
+      return (Nil, 0)
     }
-    s.map { n =>
+    var i = rowNum
+    val rows = s.map { n =>
       n.attributes foreach { attr =>
-        if (columns.contains(attr.key)) {
-          columns(attr.key).values :+ attr.value.text
-        } else {
-          columns(attr.key) = Column(attr.key, List(ColumnValue(rowNum + 1, attr.value.text)))
+        if (attr.key != "text") {
+          if (columns.contains(attr.key)) {
+            columns(attr.key).values :+ attr.value.text
+          } else {
+            columns(attr.key) = Column(attr.key, List(ColumnValue(i, attr.value.text)))
+          }
         }
       }
       val note = (n \ "_note").text
-      val children = parseOpml(n \ "outline", rowNum + 1, level + 1)
-      Row((n \ "@text").text, note, children, level + 1)
+      val (children, j) = parseOpml(n \ "outline", i + 1, level + 1)
+      val row = Row((n \ "@text").text, note, children, i, level)
+      i += j + 1
+      row
     }
+    (rows, i - rowNum)
+  }
+
+  var i = 1
+
+  def writeRows(sheet: HSSFSheet, rows: Seq[Row]): Unit = {
+    rows foreach { r =>
+      if (r.rowNum > 0) {
+        val row = sheet.createRow(r.rowNum)
+        val nameCell = row.createCell(0)
+        nameCell.setCellValue(" " * r.level * 4 + r.name)
+        i = 1
+        columns.values foreach { c =>
+          val cell = row.createCell(i)
+          cell.setCellValue(c.getValue(r.rowNum))
+          i += 1
+        }
+      }
+      if (!r.children.isEmpty) {
+        writeRows(sheet, r.children)
+        sheet.groupRow(r.rowNum + 1, r.rowNum + r.children.length)
+      }
+    }
+  }
+
+  def writeExcel(title: String, rows: Seq[Row]) = {
+    val wb = new HSSFWorkbook()
+    val sheet = wb.createSheet(title)
+    sheet.setRowSumsBelow(false)
+    val headerRow = sheet.createRow(0)
+    val nameCell = headerRow.createCell(0)
+    nameCell.setCellValue("Title")
+    columns.keys foreach { colName =>
+      val cell = headerRow.createCell(i)
+      cell.setCellValue(colName)
+      i += 1
+    }
+    writeRows(sheet, rows)
+    val out = new FileOutputStream("test.xls")
+    wb.write(out)
+    out.close()
   }
 
   println("Converting OPML file to Excel")
@@ -42,9 +99,10 @@ object Opml2Excel extends App {
   val title = (xml \ "head" \ "title").text
   println(title)
   //println(xml)
-  val rows = parseOpml(xml \ "body", 0, 0)
+  val (rows, k) = parseOpml(xml \ "body", 0, -1)
   rows foreach { row =>
     println(row)
   }
+  writeExcel(title, rows)
 
 }
